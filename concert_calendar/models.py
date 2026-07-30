939004ff8038baa7cb3280cf4ca376f33963fa6b
Eric Champarnaud <eric@electriceyerock.com>
2026-07-30 13:41:40 +0200

    Add concert event model

---
+++ concert_calendar/models.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from typing import Optional@dataclass
+class ConcertEvent:
+    date: str
+    headliner: str
+    venue: str
+    city: str
+    department: str
+
+    openers: Optional[list[str]] = None
+    genre: Optional[str] = None
+    facebook_event_url: Optional[str] = None
+    ticket_url: Optional[str] = None
