596e8c01b178c48a5c3f33a002bdd0bc37e4621c
Eric Champarnaud <eric@electriceyerock.com>
2026-07-30 13:44:02 +0200

    Add promoters to concert event model

--- concert_calendar/models.py
+++ concert_calendar/models.py
@@ -8,6 +8,7 @@ class ConcertEvent:
     department: str
 
     openers: Optional[list[str]] = None
+    promoters: Optional[list[str]] = None
     genre: Optional[str] = None
     facebook_event_url: Optional[str] = None
     ticket_url: Optional[str] = None
